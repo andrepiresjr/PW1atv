b00e8d2eb284db3e51c0cc95d832592e24dfda9b
andrepiresjr <moslekos.junior.rafa@gmail.com>
2026-06-18 22:34:18 -0300

    Atividade 18/06

---
+++ js/atv18/app.js
@@ -0,0 +1,142 @@
+//console.log("Seja bem vindo")
+
+//variavei
+/*
+
+
+let num = 10
+let num1 = 10.5
+let nome = "Pirex"
+let verdade = true
+let numero
+let numero1 = null
+console.log( typeof num1 )
+console.log( typeof num1 )
+console.log( typeof nome )
+console.log( typeof verdade )
+console.log( typeof nome )
+console.log( typeof numero )
+console.log( typeof numero1 )
+
+ */
+/*
+let num1 = Number(prompt(" Escreva o primeiro numero "))
+let num2 = Number(prompt(" Escreva o segundo numero "))
+let soma = num1 + num2
+console.log(soma)
+
+let texto = String(soma)
+console.log(typeof texto)
+
+
+//operados matematicos
++ soma
+- subtraçao
+/ divisao
+* multiplicaçao
+% MOD ou resto da divisao
+** potenciaçao
+
+*/
+/*
+let num1 = 10
+let num2 = 5
+
+let soma = num1 + num2
+let sub = num1 - num2
+let multi = num1 * num2
+let div = num1 / num2
+let exp = num1 ** num2
+let rest = num1 % num2
+
+console.log( soma, sub, multi, div, exp, rest)
+*/
+/*
+let logado = true 
+let admin = false
+
+console.log(logado && admin)
+*/
+/*
+let idade = 17
+if(idade >= 18){
+    console.log("Maior de idade")
+}else{
+    console.log("Menor de idade")
+}
+*/
+
+/*
+let dia = 2
+switch(dia){
+    case 1:
+        console.log("Domingo")
+    break
+    case 2:
+        console.log("Segunda")
+        break
+    default: console.log("Entrada invalida")
+}
+*/
+
+/*
+for(let i = 0; i <= 5; i++){
+    console.log("O valor de i é: " +i)
+}
+
+let condicao = 0
+
+while(condicao <= 5){
+    console.log("Contagem progressiva: " + condicao)
+    condicao++
+}
+
+let numero = 5
+do{
+    console.log(numero)
+    numero--
+}while(numero >= -1 )
+*/
+
+/*
+function saudacao(){
+    console.log("Seja bem vindo a minha aula de JS")
+}
+
+saudacao()
+
+function somar(num1, num2){
+    let soma = num1 + num2
+    return console.log(soma)
+}
+
+somar(20,395)
+*/
+
+/*
+function resto(){
+    console.log("Seja bem vindo a minha aula de JS")
+}
+
+resto()
+
+function somar(num1, num2){
+    let rest = num1 % num2
+    return console.log(rest)
+}
+
+somar(112,23)
+*/
+
+/*
+let frutas = ["maça", "banana", "laranja"]
+
+console.log(frutas)
+console.log(frutas.length)
+
+frutas.push('')
+*/
+
+
+
+
